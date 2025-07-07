9c9a33b38ec824228fc9ad31dece93b4e0657557
bactxse180308 <bactxse180308@fpt.edu.com>
2025-07-07 22:50:30 +0700

    aaaa

---
+++ src/main/java/hsf302/assignment/main/Hsf302Application.java
@@ -0,0 +1,13 @@
+package hsf302.assignment.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hsf302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hsf302Application.class, args);
+    }
+
+}
