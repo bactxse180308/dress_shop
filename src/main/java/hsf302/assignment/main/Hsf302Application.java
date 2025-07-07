aba4c5230ad4c85d6103756fb5f8a5f71faa6833
bactxse180308 <bactxse180308@fpt.edu.com>
2025-07-07 22:54:09 +0700

    Your commit message

--- src/main/java/hsf302/assignment/main/Hsf302Application.java
+++ src/main/java/hsf302/assignment/main/Hsf302Application.java
@@ -1,13 +1,18 @@
 package hsf302.assignment.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hsf302Application {
+public class Hsf302Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Hsf302Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 }
