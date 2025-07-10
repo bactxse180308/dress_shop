731ee2d51c58c615b9cb5b63af4d53f26816b9d3
Yakimo <lequanghung471@gmail.com>
2025-07-10 17:00:52 +0700

    fix: add dependency thymeleaf

--- src/test/java/hsf302/assignment/Hsf302ApplicationTests.java
+++ src/test/java/hsf302/assignment/Hsf302ApplicationTests.java
@@ -1,13 +1,13 @@
-package hsf302.assignment;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Hsf302ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package hsf302.assignment;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Hsf302ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
